a206b30b04c82ffb9a691a651fd6759c77f8c5e9
Maksym Kharkavyi <maksym.kharkavyi@datarobot.com>
2023-12-05 14:53:01 +0200

    lesson-04 complete

--- lesson-04/main.tf
+++ lesson-04/main.tf
@@ -16,6 +16,70 @@ provider "aws" {
 }
 
 
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_instance" "lesson_04" {
+  ami           = "ami-0c7c4e3c6b4941f0f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    aws_security_group.sg_ssh.id,
+    aws_security_group.sg_https.id
+  ]
+
+  tags = {
+    Name = "Lesson-04-VM-SG"
+  }
+}
+
+resource "aws_security_group" "sg_ssh" {
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
+resource "aws_security_group" "sg_https" {
+  ingress {
+    cidr_blocks = ["192.168.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
+
+
+
+
+
+
+
+
+/*
+
+
+
 resource "aws_instance" "bipolar-bits-application" {
   ami                    = "ami-0c7c4e3c6b4941f0f"
   instance_type          = "t2.micro"
@@ -24,13 +88,6 @@ resource "aws_instance" "bipolar-bits-application" {
     Name = "BipolarBits_application"
   }
 }
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
-
 
 resource "aws_security_group" "application-security-group" {
   name        = "application security group"
@@ -66,3 +123,5 @@ resource "aws_security_group_rule" "allow_ssh" {
   cidr_blocks       = [aws_default_vpc.default.cidr_block]
   security_group_id = aws_security_group.application-security-group.id
 }
+
+*/
